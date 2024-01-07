435874a921436694f5061631dc33173e3bab9769
KacperPuda <125309126+KacperPuda@users.noreply.github.com>
2024-01-07 14:35:53 +0100

    project add

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 STACK_NAME="BE_184725"
 
 echo "Deploying the app..."
-/bin/bash ./init_db.sh
-docker stack deploy -c ./compose/docker-compose.yml $STACK_NAME --with-registry-auth
+./init_db.sh
+docker stack deploy -c ./docker-compose.yml $STACK_NAME --with-registry-auth
